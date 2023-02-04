6d174d3df66b997b441e26bda4ed9bdcb66b3dd7
e-terven <dparentco@gmail.com>
2023-02-04 22:20:38 +0900

    test_done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,21 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean_1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean_1.getMessage());
+        HelloWorld bean_2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean_2.getMessage());
+        Cat bean_3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_3.getMessage());
+        Cat bean_4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_4.getMessage());
+
+        System.out.println(bean_1.equals(bean_2));
+        System.out.println(bean_3.equals(bean_4));
+
     }
 }
